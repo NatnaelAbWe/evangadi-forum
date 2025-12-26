85f98b7d82bd89cc75528fda5650224b0069c029
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-12-26 23:20:55 +0300

    feat: foler structure

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,19 @@
-const { sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const sequelize = new sequelize("my_db", "username", "password", {
+const sequelize = new Sequelize("my_db", "username", "password", {
   host: "localhost",
   dialect: "mysql",
 });
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected sucessfully");
+  } catch (err) {
+    console.error("unable to connect to the database", err);
+  }
+};
+
+connectDB();
 
 module.exports = sequelize;
